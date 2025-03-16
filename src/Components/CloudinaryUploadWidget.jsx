787880a67c014870ec5c896c878a474516341fd3
derya <lacin>
2025-03-17 00:04:05 +0100

    ee

--- src/Components/CloudinaryUploadWidget.jsx
+++ src/Components/CloudinaryUploadWidget.jsx
@@ -1,19 +1,21 @@
+import React, { useState } from "react";
+
 const CloudinaryUploadWidget = ({ setVideoUrl }) => {
   const handleUpload = () => {
     // Cloudinary widget'ını başlatıyoruz
     window.cloudinary.openUploadWidget(
       {
-        cloudName: process.env.REACT_APP_CLOUD_NAME, // Cloudinary hesabınızın adı
-        uploadPreset: process.env.REACT_APP_UPLOAD_PRESET, // Cloudinary üzerinde oluşturduğunuz upload preset
+        cloudName: "dbkdulf98", 
+        uploadPreset: "my_preset", 
         sources: ['local', 'url', 'camera'],
-        resourceType: "video", // Video yüklemesi yapmak için
-        multiple: false, // Birden fazla yükleme istemiyorsak false
-        clientAllowedFormats: ["mp4", "mov", "avi", "flv", "mkv"], // Yüklenebilir video formatları
+        resourceType: "video", 
+        multiple: false, 
+        clientAllowedFormats: ["mp4", "mov", "avi", "flv", "mkv"], 
       },
       (error, result) => {
         if (result && result.event === "success") {
-          const videoUrl = result.info.secure_url; // Yüklenen video URL'si
-          setVideoUrl(videoUrl); // URL'yi üst bileşene gönder
+          const videoUrl = result.info.secure_url; 
+          setVideoUrl(videoUrl); 
         }
       }
     );
